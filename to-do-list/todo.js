89e525817e0399d150d2b59b8dcdb14cfc3302c4
Patelharshal02 <109464421+Patelharshal02@users.noreply.github.com>
2024-05-28 11:25:10 +0530

    Add files via upload

---
+++ to-do-list/todo.js
@@ -0,0 +1,17 @@
+let inputs=document.getElementById("inp");
+let text=document.querySelector(".text");
+function Add(){
+    if(inputs.value ==""){
+        alert("Please Enter Task")
+    }else{
+        let newele=document.createElement("ul");
+        newele.innerHTML=`${inputs.value}<i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newele);
+        inputs.value="";
+        newele.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newele.remove()
+
+        }
+    }
+}
